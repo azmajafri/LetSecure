75fc3848ae8d7753a34297a039fb274440af61f4
azmajafri <61407560+azmajafri@users.noreply.github.com>
2021-03-05 09:25:47 +0800

    Add files via upload

--- lib/SecurityInfoPage.dart
+++ lib/SecurityInfoPage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:let_secure/HomePage.dart';
 
 
 class SecurityInfo extends StatelessWidget {
@@ -20,11 +22,17 @@ class SecurityInfoPage extends StatefulWidget {
 class _SecurityInfoPageState extends State<SecurityInfoPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.blue[900],
       appBar: AppBar(
         backgroundColor: Colors.grey.withOpacity(0.6),
         leading: IconButton(
+          onPressed: () {
+            MaterialPageRoute(
+                builder: (_) =>HomePage(),
+            );
+          },
           icon: Icon(
             Icons.arrow_back_rounded,
             color: Colors.white,
@@ -36,7 +44,155 @@ class _SecurityInfoPageState extends State<SecurityInfoPage> {
       ),
       body: SingleChildScrollView(
         child: Container(
-
+          padding: EdgeInsets.only(top: 50.0),
+          child:Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+                height: 100,
+                width: 370,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.07),
+                      blurRadius: 4,
+                      spreadRadius: 6,
+                    )
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Image(image: AssetImage('images/cyberbullying.png'),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30.0, 0, 30.0, 10.0),
+                        child: Text("Cyberbullying",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 100,
+                width: 370,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.07),
+                      blurRadius: 4,
+                      spreadRadius: 6,
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Image(image: AssetImage('images/cyberattack.png'),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30.0, 0, 30.0, 10.0),
+                        child: Text("Online Cyber Stalker",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 100,
+                width: 370,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.07),
+                      blurRadius: 4,
+                      spreadRadius: 6,
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Image(image: AssetImage('images/grooming.png'),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30.0, 0, 30.0, 10.0),
+                        child: Text("Online Grooming",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                height: 100,
+                width: 370,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.07),
+                      blurRadius: 4,
+                      spreadRadius: 6,
+                    ),
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Container(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Image(image: AssetImage('images/pornography.png'),
+                        width: 70,
+                        height: 70,
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(30.0, 0, 30.0, 10.0),
+                        child: Text("Internet Pornography",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
