d0c1addd19e36d31df77f261763d7a93e7c82302
azmajafri <61407560+azmajafri@users.noreply.github.com>
2021-04-09 00:29:06 +0800

    Online Grooming Page

--- lib/screens/groomingPage.dart
+++ lib/screens/groomingPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'cybersecurityInfo.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class groomingPage extends StatefulWidget {
   @override
@@ -25,6 +26,173 @@ class _groomingPageState extends State<groomingPage> {
         ),
         title: Text('Online Grooming'),
         centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(left: 10, top: 40),
+                child: Image(image: AssetImage('images/cyberbully.jpg'),
+                  width: 115,
+                  height: 110,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 28, left: 140),
+                width: 260,
+                height: 130,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 148,
+                top: 43,
+                child: Text("When a person makes online \ncontact with a child with \nattention of established \nrelationship to enable their \nsexual abuse.",
+                  style: GoogleFonts.rubik(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 196, left: 28),
+                child: Text("Quick Tips!", style: GoogleFonts.inconsolata(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                ),
+              ),
+              Positioned(
+                top: 180,
+                left: 154,
+                child: Image(image: AssetImage('images/lightbulb.png'),
+                  width: 35,
+                  height: 35,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 238, left: 25),
+                width: 364,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 55,
+                top: 253,
+                child: Text("Parents should know with \nwhom their children be friends with.",
+                  style: GoogleFonts.rubik(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 332, left: 25),
+                width: 364,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 78,
+                top: 360,
+                child: Text("Stay safe online and in real life.",
+                  style: GoogleFonts.rubik(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 428, left: 25),
+                width: 364,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 40,
+                top: 455,
+                child: Text("Encourage children to talk to someone.",
+                  style: GoogleFonts.rubik(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 524, left: 25, bottom: 30),
+                width: 364,
+                height: 75,
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.4),
+                      spreadRadius: 4,
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 30,
+                top: 542,
+                child: Text("Parents should reviewing apps, sites, and \ngames their children use.",
+                  style: GoogleFonts.rubik(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
