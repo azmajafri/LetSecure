f6c1d6818aa94d95fdac5011b7f6acdd250d3576
azmajafri <61407560+azmajafri@users.noreply.github.com>
2021-04-29 22:27:15 +0800

    mcmc page

---
+++ lib/screens/mcmc.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'cybersecOrg.dart';
+
+class mcmc extends StatefulWidget {
+  @override
+  _mcmcState createState() => _mcmcState();
+}
+
+class _mcmcState extends State<mcmc> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey.withOpacity(0.6),
+        leading: IconButton(icon: Icon(
+          Icons.arrow_back_rounded,
+          color: Colors.white,
+          size: 30,
+        ),
+          onPressed: () => Navigator.push(context, new MaterialPageRoute(
+              builder: (context) => CyberSecOrgPage())
+          ),
+        ),
+        title: Text('Malaysian Communications and \nMultimedia Commission (MCMC)'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Image(
+                    image: AssetImage('images/mcmc.jpg'),
+                    width: 150,
+                    height: 150,
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(top: 160),
+                  height: 400,
+                  width: 370,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.07),
+                        blurRadius: 4,
+                        spreadRadius: 6,
+                      ),
+                    ],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
