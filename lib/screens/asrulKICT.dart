61b1c87e35fdb3bd3429e6db91b94226c39b130f
nurainzulaikha <61407504+nurainzulaikha@users.noreply.github.com>
2021-06-17 16:41:27 +0800

    Add files via upload

---
+++ lib/screens/asrulKICT.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:letsecureapp/screens/inspirationalStories.dart';
+
+class asrulKICT extends StatefulWidget {
+  @override
+  _asrulKICTState createState() => _asrulKICTState();
+}
+
+class _asrulKICTState extends State<asrulKICT> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.blue[900],
+      appBar: AppBar(
+        backgroundColor: Colors.grey.withOpacity(0.6),
+        leading: IconButton(icon: Icon(
+          Icons.arrow_back_rounded,
+          color: Colors.white,
+          size: 30,
+        ),
+          onPressed: () => Navigator.push(context, new MaterialPageRoute(
+              builder: (context) => inspirationalStories())
+          ),
+        ),
+        title: Text('Inspirational Stories'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(45, 20, 20, 20),
+                child: Text("Successful story of Br. Asrul",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 80),
+                child: Image(image: AssetImage('assets/asrul1.png'),
+                  width:450,
+                  height: 450,
+                ),
+              ),
+              // Positioned(
+              //   right: 18,
+              //   top: 570,
+              //   child: InkWell(
+              //     onTap:(){
+              //       Navigator.push(context,new MaterialPageRoute(
+              //           builder: (context) => occupation())
+              //       );
+              //     },
+              //     child: Text("Next",
+              //       style: GoogleFonts.rubik(
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.w400,
+              //         color: Colors.white,
+              //       ),
+              //       textAlign: TextAlign.center,
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
