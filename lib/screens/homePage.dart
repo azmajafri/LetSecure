ce3111305c637e8d003213b66abdec60187d0feb
azmajafri <61407560+azmajafri@users.noreply.github.com>
2021-06-20 11:54:20 +0800

    Homepage

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:letsecureapp/screens/aboutPage.dart';
-import 'package:letsecureapp/screens/certificate.dart';
-import 'package:letsecureapp/screens/cybersecurityInfo.dart';
-import 'package:letsecureapp/screens/login_screen.dart';
-import 'package:letsecureapp/screens/stageGame.dart';
-
+import 'stageGame.dart';
+import 'aboutPage.dart';
 import 'careerPage.dart';
+import 'certificate.dart';
+import 'profile.dart';
+import 'LoginPage.dart';
+import 'cybersecurityInfo.dart';
 
+class HomePage extends StatefulWidget {
+  final String uid;
 
-
-class HomeScreen extends StatelessWidget {
+  HomePage({Key key, this.uid}) : super(key: key);
   static const routeName = '/home';
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  // @override
+  // initState() {
+  //   // this.getCurrentUser();
+  //   super.initState();
+  // }
+  //
+  // // void getCurrentUser() async {
+  // //   currentUser = await FirebaseAuth.instance.currentUser;
+  // // }
+
+  @override
   Widget build(BuildContext context) {
+    // Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.blue[900],
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey.withOpacity(0.6),
-        title: Text('LetSecure'),
+        title: Text("LetSecure"),
         centerTitle: true,
+        backgroundColor: Colors.blueGrey.withOpacity(0.6),
       ),
       drawer: Drawer(
         child: ListView(
@@ -27,6 +45,7 @@ class HomeScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Colors.blueAccent
                 ),
+
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   // mainAxisAlignment: MainAxisAlignment.end,
@@ -34,7 +53,7 @@ class HomeScreen extends StatelessWidget {
                     Container(
                       width: 65,
                       height: 65,
-                      child: Image(image: AssetImage('assets/logo.png'),
+                      child: Image(image: AssetImage('images/logo.png'),
                       ),
                     ),
                     Container(
@@ -49,27 +68,106 @@ class HomeScreen extends StatelessWidget {
                     )
                   ],
                 ),
+                // accountName: FutureBuilder(
+                //     future: FirebaseDatabase.instance
+                //         .reference().child("Users")
+                //         .child(widget.uid)
+                //         .once(),
+                //     builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                //       if (snapshot.hasData) {
+                //         return Text(snapshot.data.value['email']);
+                //       } else {
+                //         return CircularProgressIndicator();
+                //       }
+                //     }),
+
+                // accountEmail: FutureBuilder(
+                //   future: FirebaseDatabase.instance
+                //       .reference()
+                //       .child("Users")
+                //       .child(widget.uid)
+                //       .once(),
+                //   builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
+                //     if (snapshot.hasData) {
+                //       return Text(snapshot.data.value['name']);
+                //     } else {
+                //       return CircularProgressIndicator();
+                //     }
+                //   }),
+
               ),
+
+              // Column(
+              //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     // mainAxisAlignment: MainAxisAlignment.end,
+              //     children: <Widget>[
+              //       Container(
+              //         width: 65,
+              //         height: 65,
+              //         child: Image(image: AssetImage('images/logo.png'),
+              //         ),
+              //       ),
+              //       // Container(
+              //       //   margin: EdgeInsets.only(top: 16),
+              //       //   child: Text(
+              //       //     "LetSecure",
+              //       //     style: TextStyle(
+              //       //         color: Colors.white,
+              //       //         fontSize: 20
+              //       //     ),
+              //       //   ),
+              //       // )
+              //     ],
+              //   ),
+
               ListTile(
                 leading: Icon(Icons.home),
                 title: Text("Home"),
-                onTap: () => Navigator.of(context).pop(),
+                onTap: () {
+                  print(widget.uid);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage(uid: widget.uid)),
+                  );
+                }
+                // => Navigator.push(context, new MaterialPageRoute(
+                //     builder: (context) => HomePage())
+                // ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Profile"),
+                onTap: ()
+                // {
+                //   print(widget.uid);
+                //   Navigator.push(
+                //     context,
+                //     MaterialPageRoute(builder: (context) => HomePage(uid: widget.uid)),
+                //   );
+                // }
+                => Navigator.push(context, new MaterialPageRoute(
+                    builder: (context) => ProfileView())
+                ),
               ),
               ListTile(
                 leading: Icon(Icons.info),
                 title: Text("About"),
-                onTap: () => Navigator.push(context, new MaterialPageRoute(
+                onTap: ()
+                // {
+                //   print(widget.uid);
+                // }
+                => Navigator.push(context, new MaterialPageRoute(
                     builder: (context) => aboutPage())
                 ),
               ),
               ListTile(
                 leading: Icon(Icons.logout),
                 title: Text("Logout"),
-                  onTap: () => Navigator.push(context, new MaterialPageRoute(
-                      builder: (context) => LoginScreen())
+                onTap: () => Navigator.push(context, new MaterialPageRoute(
+                    builder: (context) => LoginPage())
               ),
               ),
-            ],
+            ]
         ),
       ),
       body: SingleChildScrollView(
@@ -84,7 +182,6 @@ class HomeScreen extends StatelessWidget {
                     Navigator.push(context, new MaterialPageRoute (
                         builder: (context) => SecurityInfoPage())
                     );
-
                   },
                   child: Column(
                     children: <Widget>[
@@ -105,7 +202,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         child: Column(
                           children: <Widget>[
-                            Image(image: AssetImage('assets/cybersecurity.png'),
+                            Image(image: AssetImage('images/cyberSecurity.png'),
                               width: 115,
                               height: 115,
                             ),
@@ -151,7 +248,7 @@ class HomeScreen extends StatelessWidget {
                         child: Column(
                           children: <Widget>[
 
-                            Image(image: AssetImage('assets/careerInfo.png'),
+                            Image(image: AssetImage('images/careerInfo.png'),
                               width: 105,
                               height: 105,
                             ),
@@ -176,7 +273,6 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               Container(
-
                 child: InkWell(
                   onTap: () {
                     Navigator.push(context, new MaterialPageRoute (
@@ -186,7 +282,6 @@ class HomeScreen extends StatelessWidget {
                   child: Column(
                     children: <Widget>[
                       Container(
-
                         padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 10.0),
                         height: 180,
                         width: 180,
@@ -203,7 +298,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         child: Column(
                           children: <Widget>[
-                            Image(image: AssetImage('assets/quiz.png'),
+                            Image(image: AssetImage('images/quiz.png'),
                               width: 105,
                               height: 105,
 
@@ -222,8 +317,8 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Container(
 
+              Container(
                 child: InkWell(
                   onTap: () {
                     Navigator.push(context, new MaterialPageRoute (
@@ -233,7 +328,6 @@ class HomeScreen extends StatelessWidget {
                   child: Column(
                     children: <Widget>[
                       Container(
-
                         padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 10.0),
                         height: 180,
                         width: 180,
@@ -250,7 +344,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         child: Column(
                           children: <Widget>[
-                            Image(image: AssetImage('assets/cert.png'),
+                            Image(image: AssetImage('images/cert.png'),
                               width: 105,
                               height: 105,
 
